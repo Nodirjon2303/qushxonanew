e77452b804128a3b91d5a77b67b843199cfe8984
nodirjon <ruzimurodovnodir66@gmail.com>
2022-09-22 01:50:28 +0500

    fixing

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(Client)
 admin.site.register(IncomeClient)
 admin.site.register(IncomeDehqon)
 admin.site.register(IncomeSotuvchi)
