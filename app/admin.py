f04d283c6754e945e16a8005e07a2e3aa37117ee
nodirjon <ruzimurodovnodir66@gmail.com>
2022-09-23 23:39:11 +0500

    added save functions

--- app/admin.py
+++ app/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(IncomeClient)
 admin.site.register(IncomeDehqon)
 admin.site.register(IncomeSotuvchi)
-admin.site.register(ExpenseDehqon)
+# admin.site.register(ExpenseDehqon)
 admin.site.register(ExpenseSotuvchi)
 admin.site.register(ExpenseClient)
 admin.site.register(KallaHasb)
 admin.site.register(Teri)
 
+
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'phone', 'role', 'status_bozor']
     search_fields = ['full_name', 'phone']
+
+
+@admin.register(ExpenseDehqon)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['dehqon', 'product', 'created_date']
+    search_fields = ['dehqon__full_name', 'product_name']
